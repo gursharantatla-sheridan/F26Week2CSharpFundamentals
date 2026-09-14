aa87b3c178c1e71d6149e4581fc8bcd4551ef369
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-14 12:47:36 -0400

    string interpolation

--- F26Week2CSharpFundamentals/Program.cs
+++ F26Week2CSharpFundamentals/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        static int x;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int @double = 5;
+
+            int i = 5;
+            double d = 5.5;
+
+            float f = 5.5F;
+            decimal m = 5.5M;
+            long l = 43534L;
+
+            //int x;
+            i = x + 5;
+            Console.WriteLine(i);
+
+            var n = 5;
+
+
+            Console.WriteLine(@"Hello\nworld");
+            Console.WriteLine("\n\n");
+
+
+            int cars = 5, bikes = 7, trucks = 10;
+
+            // string concatenation
+            Console.WriteLine("I have " + cars + " cars, " + bikes + " bikes and " + trucks + " trucks");
+
+            // string interpolation
+            Console.WriteLine($"I have {cars} cars, {bikes} bikes and {trucks} trucks");
         }
     }
 }
