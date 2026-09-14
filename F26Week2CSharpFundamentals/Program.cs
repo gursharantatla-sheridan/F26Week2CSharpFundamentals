10ffac5ca35a07fc4ba939f1ee9327552558e7a2
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-14 13:19:00 -0400

    strings

--- F26Week2CSharpFundamentals/Program.cs
+++ F26Week2CSharpFundamentals/Program.cs
@@ -33,6 +33,58 @@
 
             // string interpolation
             Console.WriteLine($"I have {cars} cars, {bikes} bikes and {trucks} trucks");
+
+            Console.WriteLine("\n\n");
+
+
+
+            // value and reference types
+
+            int a = 5;
+            int b = a;
+
+            a++;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("\n");
+
+
+            Circle c1 = new Circle();
+            Console.WriteLine(c1.radius);
+
+            c1.radius = 50;
+
+            Circle c2 = c1;
+            //Circle c2 = new Circle();
+
+            Console.WriteLine(c1.radius);
+            Console.WriteLine(c2.radius);
+
+            c2.radius = 100;
+
+            Console.WriteLine(c1.radius);
+            Console.WriteLine(c2.radius);
+
+            Console.WriteLine("\n\n");
+
+
+            // string example
+            string s1 = "hello";
+            string s2 = s1;
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            s2 = "bye";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
+    }
+
+    class Circle
+    {
+        public int radius;
     }
 }
